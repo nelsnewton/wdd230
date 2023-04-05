958cb17c725165cac032c7b1f54aef48df1d318e
nelsnewton <isacn5272@gmail.com>
2023-04-05 17:23:55 +0300

    final

--- final-project/js/prophets.js
+++ final-project/js/prophets.js
@@ -1,4 +1,4 @@
-const requestURL = 'final-project/product.js';
+const requestURL = 'https://nelsnewton.github.io/wdd230/final-project/product.json';
 
 
 fetch(requestURL).then(function (response) {return response.json();})
