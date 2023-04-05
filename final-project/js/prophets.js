73cf57210382074cee15b7e34481828ebb4c42de
nelsnewton <isacn5272@gmail.com>
2023-04-05 17:26:49 +0300

    final

--- final-project/js/prophets.js
+++ final-project/js/prophets.js
@@ -47,15 +47,3 @@ function displayFruits(fruit) {
 
   document.querySelector('.cards').appendChild(card);
 }
-
-
-//dates
-const yearDisplay = document.querySelector('footer h2');
-var today = new Date();
-var todayYear = today.getFullYear();
-//copyright
-yearDisplay.innerHTML = `&copy; ${todayYear} - Tumwesige Nelson`;
-//modDate
-const modDateDisplay = document.getElementById('modDate');
-let modDate = document.lastModified;
-modDateDisplay.textContent = ("Last modified "+modDate);
