6103050c8032affbeeea5463157eee28e02ede59
csenelson <cseprojects210@gmail.com>
2023-04-30 15:36:02 +0300

    preps 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        Console.Write("What is your last name? ");
+
+        string first = Console.ReadLine();
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Hello {first}, this is your output");
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello Welcome to Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int percentage = int.parse(userAnswer);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+       else
+       {
+        letter = "F";
+       }
+
+       Console.WriteLine($"Your grade is: {letter}");
+
+       if (percentage >= 70)
+       {
+        Console.WriteLine("You have passed");
+       }
+       else if (percentage >= 55)
+       {
+        Console.WriteLine("Good trial, more effort needed");
+       }
+       else 
+       {
+        Console.WriteLine("Aim higher, next time better.");
+       }
+
     }
 }
